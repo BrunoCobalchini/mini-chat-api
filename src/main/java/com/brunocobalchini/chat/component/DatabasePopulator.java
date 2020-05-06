c7a13239d826851e39dcbe88743d21eed15df3c4
Gabriel <Raoni.Gabriel@mastercard.com>
2020-05-06 13:19:45 -0300

    first commit

---
+++ src/main/java/com/brunocobalchini/chat/component/DatabasePopulator.java
@@ -0,0 +1,24 @@
+package com.brunocobalchini.chat.component;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabasePopulator {
+
+	@PostConstruct
+	public void populate() {
+		// TODO: create user "Nick Fury" and save to the database
+		// TODO: create user "Tony Stark" and save to the database
+		// TODO: create user "Steve Rogers" and save to the database
+
+		// TODO: Create conversation with those 3 users and save to the database
+
+		// TODO: Create Message from user "Nick Fury" with content: "Avengers, assemble!" and save to the database
+		// TODO: Create Message from user "Tony Stark" with content: "I am Iron Man." and save to the database
+		// TODO: Create Message from user "Tony Stark" with content: "I Can Do This All Day." and save to the database
+
+	}
+
+}
