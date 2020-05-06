b9c7583e25b3cff2b96e18f099aaea4d40a2d1eb
Gabriel <Raoni.Gabriel@mastercard.com>
2020-05-06 13:11:31 -0300

    added BCrypt as default password encryptor

--- src/main/java/com/brunocobalchini/chat/MiniChatApp.java
+++ src/main/java/com/brunocobalchini/chat/MiniChatApp.java
@@ -2,9 +2,17 @@ package com.brunocobalchini.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MiniChatApp {
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiniChatApp.class, args);
